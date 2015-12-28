7eb0c88562c1917c9147f96cbc5d06f558e9dae5
Becky Torrey <torrey1028@gmail.com>
2015-12-28 01:12:25 -0800

    Added basic project structure w/ no actual code

---
+++ main.c
@@ -0,0 +1,15 @@
+/* stm32f4_discovery.h is located in Utilities/STM32F4-Discovery
+ * and defines the GPIO Pins where the leds are connected.
+ * Including this header also includes stm32f4xx.h and
+ * stm32f4xx_conf.h, which includes stm32f4xx_gpio.h
+ */
+#include "stm32f4_discovery.h"
+
+/* The main function is called from the startup code in file
+ * Libraries/CMSIS/ST/STM32F4xx/Source/Templates/TrueSTUDIO/startup_stm32f4xx.s
+ * (line 101)
+ */
+int main(void)
+{
+    return 0; 
+}
