b30e670256672e2ce9a7463d9ee96e2d2c7c67f3
Becky Torrey <torrey1028@gmail.com>
2015-12-29 15:42:17 -0800

    Added code using GESTURES_GetGesture()

--- main.c
+++ main.c
@@ -9,36 +9,40 @@
  */
 int main(void)
 {
-	TM_LIS302DL_LIS3DSH_t Axes_Data;
-	
 	GESTURES_Init();	
 
 	while (1) {
-		/* Read axes data from initialized accelerometer */
-		TM_LIS302DL_LIS3DSH_ReadAxes(&Axes_Data);
+		
+		/* Get current gesture */
+		GESTURE_t gesture = GESTURES_GetGesture();
+		
+		/* Light correct LED */
+		switch (gesture) {
 
-		/* Turn LEDS on or off */
-		/* Check X axes */
-		if (Axes_Data.X > 200) {
-			TM_DISCO_LedOn(LED_RED);
-		} else {
-			TM_DISCO_LedOff(LED_RED);
-		}
-		if (Axes_Data.X <-200) {
-			TM_DISCO_LedOn(LED_GREEN);
-		} else {
-			TM_DISCO_LedOff(LED_GREEN);
-		}
-		/* Check Y axes */
-		if (Axes_Data.Y > 200) {
-			TM_DISCO_LedOn(LED_ORANGE);
-		} else {
-			TM_DISCO_LedOff(LED_ORANGE);
-		}
-		if (Axes_Data.Y < -200) {
-			TM_DISCO_LedOn(LED_BLUE);
-		} else {
-			TM_DISCO_LedOff(LED_BLUE);
+			case RIGHT: 
+				TM_DISCO_LedOn(LED_GREEN);
+				TM_DISCO_LedOff(LED_RED | LED_BLUE | LED_ORANGE);
+				break;
+		
+			case LEFT:			
+				TM_DISCO_LedOn(LED_RED);
+				TM_DISCO_LedOff(LED_GREEN | LED_BLUE | LED_ORANGE);
+				break;
+		
+			case FORWARD:
+				TM_DISCO_LedOn(LED_BLUE);
+				TM_DISCO_LedOff(LED_GREEN | LED_RED | LED_ORANGE);
+				break;
+			
+			case BACK:	
+				TM_DISCO_LedOn(LED_ORANGE);
+				TM_DISCO_LedOff(LED_GREEN | LED_RED | LED_BLUE);
+				break;
+		
+			case BAD:
+				TM_DISCO_LedOn(LED_GREEN | LED_RED | LED_ORANGE | LED_BLUE);	
+				break;
+
 		}
 	}       
 }
